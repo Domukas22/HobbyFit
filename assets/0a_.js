ee101596cb1f30ff4c78f2c17c027c61f4aacb16
Domas <114532410+Domukas22@users.noreply.github.com>
2023-10-02 20:32:18 +0200

    Clean html syntax. Start cleaning cart page js

--- assets/0a_.js
+++ assets/0a_.js
@@ -1,15 +1,3 @@
-// errors and alerts + qnt btn texts
-// finish qnt input design
-// fix swiper --> swiper wrapper needs to have a fixed, px width
-// clean up code
-
-// qnt submit button doesnt appear on type
-// see cart infos on load, and display/hide add to cart buttons based on parent class
-// qnt selection incorrectly calculates the final price inside btn
 // shopping cart btn in the nav only works if the bag icon is clicked
-
-
-// onkeydown="QNT.HANDLEmobileGoBtnClick(event, this);"
-
-
-.
+// clean cart page
+// clean filter page

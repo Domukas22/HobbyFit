a0352e5f38f1fb4e7e400a7910b81c99b0ab365d
Domas <114532410+Domukas22@users.noreply.github.com>
2023-10-02 14:45:00 +0200

    Product page code restructure

--- assets/0a_.js
+++ assets/0a_.js
@@ -1,31 +1,15 @@
-function _CLOSEqntInput() {
-  const qntInputPARENT = document.querySelector(".input_box.qnt");
-  const qntInputFRONT = document.querySelector(".quantity_front");
-  const qntInputBACK = document.querySelector(".quantity_back");
+// errors and alerts + qnt btn texts
+// finish qnt input design
+// fix swiper --> swiper wrapper needs to have a fixed, px width
+// clean up code
 
-  console.log("ss");
-  qntInputPARENT.dataset.box = "closed";
-  qntInputBACK.style.opacity = "0%";
-  qntInputFRONT.style.display = "flex";
-  setTimeout(() => {
-    qntInputFRONT.style.opacity = "100%";
-  }, 70);
-  setTimeout(() => {
-    qntInputBACK.style.display = "none";
-  }, 151);
-  qntInputPARENT.style.height = "54px";
-}
+// qnt submit button doesnt appear on type
+// see cart infos on load, and display/hide add to cart buttons based on parent class
+// qnt selection incorrectly calculates the final price inside btn
+// shopping cart btn in the nav only works if the bag icon is clicked
 
-// on load
-// QNT.GETqntInfo
-// QNT.EDITEDITtextQnts
-// QNT.HANDLEqntOptions
 
-function ISvariantInBag(target_id) {
-  const targetITEM = cartITEMS.find((x) => x.id == target_id);
-  return {
-    inCart: !!targetITEM ? targetITEM : false,
-    qnt: !!targetITEM ? targetITEM.quantity : 0,
-  };
-}
-return { IScartEmpty, ISvariantInBag };
+// onkeydown="QNT.HANDLEmobileGoBtnClick(event, this);"
+
+
+.

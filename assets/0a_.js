3cf699ed0c73ff507a2bc167932ad9f5e07c7a0e
Domas <114532410+Domukas22@users.noreply.github.com>
2023-10-05 18:20:46 +0200

    removed theme_id quotes in the .yml file

--- assets/0a_.js
+++ assets/0a_.js
@@ -1,6 +1,7 @@
 // redesign home hero
 // connect to github and learn to make changes
 // fix cart bug when cart link is english (product page "view cart" btn)
+// translate 404 page
 
 // add payments
 // edit payment icons

dfe2636475eb60a61fbf142de46c9f289fdf71f9
Domas <114532410+Domukas22@users.noreply.github.com>
2023-10-05 17:59:19 +0200

    deleted random file

--- assets/0a_.js
+++ assets/0a_.js
@@ -1,5 +1,6 @@
 // redesign home hero
 // connect to github and learn to make changes
+// fix cart bug when cart link is english (product page "view cart" btn)
 
 // add payments
 // edit payment icons

1ee4e98234147a556f85badd8dc7ff6b043cd92d
Domas <114532410+Domukas22@users.noreply.github.com>
2023-10-08 17:50:27 +0200

    Fix bugs

--- assets/12_localization.js
+++ assets/12_localization.js
@@ -2,9 +2,9 @@
   // console.log(window.Shopify)
 
   // is cookie set?
-  const cookie_country = get_cookie("user_country");
-  const cookie_lang = get_cookie("user_lang");
-  if (!cookie_country && !cookie_lang) {
+  const countryCOOKIE = GETcookie("user_country");
+  const languageCookie = GETcookie("user_lang");
+  if (!countryCOOKIE && !languageCookie) {
     localize();
   }
 
@@ -26,38 +26,35 @@
           lang = "en";
         }
 
-        set_cookie("user_country", country, 14);
-        set_cookie("user_lang", lang, 14);
-        submit_localizationForm(country, lang);
+        SETcookie("user_country", country, 14);
+        SETcookie("user_lang", lang, 14);
+        SUBMITlocalizationForm(country, lang);
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   }
-  function submit_localizationForm(country, lang) {
+  function SUBMITlocalizationForm(country, lang) {
     const form = document.querySelector(".localization_form");
     const input_return = form.querySelector("input[name=return_to]");
     const input_country = form.querySelector("input[name=country_code]");
     const input_lang = form.querySelector("input[name=language_code]");
 
-    form.setAttribute("action", get_formAction(get_lang()));
-    input_return.value = create_langUrl(
-      get_lang(),
+    form.setAttribute("action", GETformAction(GETcurrentLang()));
+    input_return.value = CREATEurl(
+      GETcurrentLang(),
       lang,
       window.location.href
     ).replace("https://sportokrepselis.myshopify.com", "");
 
-    console.log(input_return.value);
-
     input_country.value = country;
     input_lang.value = lang;
 
-    console.log(form);
     form.submit();
 
     //document.querySelector('form[action="/localization"]').submit()
   }
-  function get_formAction(lang) {
+  function GETformAction(lang) {
     if (lang == "en") {
       return "/localization";
     } else {
@@ -65,16 +62,7 @@
     }
   }
 
-  function set_cookie(name, value, days) {
-    const expires = new Date();
-    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
-  }
-  function get_cookie(name) {
-    const cookieValue = document.cookie.match(`(^|;)\\s*${name}=([^;]+)`);
-    return cookieValue ? cookieValue[2] : null;
-  }
-  function get_lang() {
+  function GETcurrentLang() {
     if (window.location.href.includes(".com/de")) {
       return "de";
     } else if (window.location.href.includes(".com/lt")) {
@@ -86,11 +74,9 @@
       return "en";
     }
   }
-  function create_langUrl(lang_current, lang_target, url) {
+  function CREATEurl(lang_current, lang_target, url) {
     const langTarget_new = lang_target == "en" ? "" : lang_target;
 
-    console.log(lang_current);
-    console.log(langTarget_new);
     if (lang_current == "en" && langTarget_new == "") {
       //if EN
       return url;
@@ -103,14 +89,28 @@
     }
   }
 
+  function SETcookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+  }
+  function GETcookie(name) {
+    const cookieValue = document.cookie.match(`(^|;)\\s*${name}=([^;]+)`);
+    return cookieValue ? cookieValue[2] : null;
+  }
+  function SELECTlanguage(params) {
+    // SETcookie(name, value, days)
+    // SUBMITlocalizationForm(country, lang);
+  }
+
   const langOptions_all = document.querySelectorAll(".langLink");
   const countryOptions_all = document.querySelectorAll(".countryOption");
   langOptions_all.forEach((x) =>
     x.addEventListener("click", (e) => {
       let lang_target = e.currentTarget.dataset.lang;
-      set_cookie("user_lang", lang_target, 14);
-      window.location.href = create_langUrl(
-        get_lang(),
+      SETcookie("user_lang", lang_target, 14);
+      window.location.href = CREATEurl(
+        GETcurrentLang(),
         lang_target,
         window.location.href
       );
